1fc23f5db5336e4a5e4dd4c70759ac801fd89e20
Elias Pozewaunig <64259275+elpozewaunig@users.noreply.github.com>
2024-07-19 20:11:05 +0200

    Implemented basic jumping to next line

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -6,12 +6,20 @@ extends PathFollow2D
 var speed = 100
 var zoom_fact = 0.001
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	progress += delta * speed * game_manager.tick_speed
+	var change = delta * speed * game_manager.tick_speed
+	var change_ratio  = change / get_parent().curve.get_baked_length()
+	if progress_ratio + change_ratio > 1:
+		progress += change
+		var next = get_parent().get_child(1)
+		reparent(next)
+	else:
+		progress += change
 	camera.zoom.x = 1 / (speed * game_manager.tick_speed * zoom_fact + 1)
 	camera.zoom.y = 1 / (speed * game_manager.tick_speed * zoom_fact + 1)
