a6d7b3fd331df178ebc0962938171dbe5b8a352d
Elias Pozewaunig <64259275+elpozewaunig@users.noreply.github.com>
2024-07-19 20:46:27 +0200

    Implemented basic lane switching

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -5,7 +5,7 @@ extends PathFollow2D
 
 var speed = 100
 var zoom_fact = 0.001
-
+var current_lane = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,6 +13,15 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if Input.is_action_just_pressed("ui_left"):
+		if current_lane > 0:
+			current_lane -= 1
+			reparent(game_manager.get_child(current_lane))
+	if Input.is_action_just_pressed("ui_right"):
+		if current_lane < 2:
+			current_lane += 1
+			reparent(game_manager.get_child(current_lane))
+	
 	var change = delta * speed * game_manager.tick_speed
 	var change_ratio  = change / get_parent().curve.get_baked_length()
 	if progress_ratio + change_ratio >= 1:
@@ -24,3 +33,4 @@ func _process(delta):
 		progress += change
 	camera.zoom.x = 1 / (speed * game_manager.tick_speed * zoom_fact + 1)
 	camera.zoom.y = 1 / (speed * game_manager.tick_speed * zoom_fact + 1)
+		
