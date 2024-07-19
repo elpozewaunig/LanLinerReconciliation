7538e96d3f51bcd89d82851d5a0fb206b07d1cc8
Elias Pozewaunig <64259275+elpozewaunig@users.noreply.github.com>
2024-07-19 21:41:56 +0200

    Seal fix
    Co-Authored-By: Seal <88384688+Seal187@users.noreply.github.com>

--- scenes/PathBuilder.gd
+++ scenes/PathBuilder.gd
@@ -1,11 +1,10 @@
 extends Node2D
 
-
 func createPath():
 	var scene = load("res://scenes/visible_path.tscn")
 	var path = scene.instantiate()
-	var curve = Curve2D.new()
-	curve.add_point(Vector2(0,100))
-	curve.add_point(Vector2(100,200))
+	path.curve = Curve2D.new()
+	path.curve.add_point(Vector2(0,100))
+	path.curve.add_point(Vector2(100,200))
 
-	return curve
+	return path
