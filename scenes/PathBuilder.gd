c5df9c9d37d58bdc5356e876ebcbdfc5a712968b
Seal <88384688+Seal187@users.noreply.github.com>
2024-07-20 00:45:51 +0200

    Lane Changes

--- scenes/PathBuilder.gd
+++ scenes/PathBuilder.gd
@@ -1,27 +1,92 @@
 extends Node2D
 
-var pl = 500
+
+var pl = 250 # how long is a path-line in px
+var gap = 80 # how many px gap between the duplis 
+var ec = 3 # how many path elements
+var lpp = 2 # how many lines per path? (additional to asfo)
+var asfo = 2 # how many lines after a split to fade out into a normal path
 var rng = RandomNumberGenerator.new()
 
 func createPath():
-	var root = getInstance()
+	var root = Node2D.new()
+	var lane = Node2D.new()
+	var neuepath = rec(ec, Vector2(0,0),"nofade")
+	lane.add_child(neuepath)
+	lane.name = "lane0"
+	root.add_child(lane)
+	for i in 4:
+		var dupli = lane.duplicate()
+		dupli.position = Vector2(dupli.position.x-(gap*(i+1)), dupli.position.y)
+		dupli.name = "lane"+str(i+1)
+		root.add_child(dupli)
+	
 	root.name = "root"
-	root.curve.add_point(Vector2(0,-pl))
-	root.add_child(rec(5, Vector2(0,-pl)))
 	return root
 
-func rec(recursionDepth: int, origin: Vector2):
+func rec(recursionDepth: int, origin: Vector2, name:String):
 	var path = getInstance()
-	path.name = "LChild" 
 	
-	var rdmNum = rng.randi_range(-1,1)
-	
+
 	path.position = Vector2(origin.x,origin.y)
-	path.curve.add_point(Vector2(rdmNum*pl,-pl))
-	var newOrigin = Vector2(origin.x+(rdmNum*pl), origin.y-pl)
+	path.curve.add_point(Vector2(0,0))
+	var prevlr = 0
+	
+	var fadedist = asfo*pl
+	fadedist = asfo*pl
+	if(name=="LChild"):
+		for i in asfo:
+			path.curve.add_point(Vector2(-(i+1)*pl,-(pl*(i+1))))	
+		prevlr += -1*asfo
+	elif(name=="RChild"):
+		for i in asfo:
+			path.curve.add_point(Vector2((i+1)*pl,-(pl*(i+1))))	
+		prevlr += 1*asfo
+		
+	elif(name=="SChild"):
+		for i in asfo:
+			path.curve.add_point(Vector2(0,-((i+1)*pl)))
+		
+	else:
+		fadedist = 0
+		path.curve.add_point(Vector2(0,0))
+	
+	
+	for i in lpp:
+		var rdmNum = rng.randi_range(-1,1)
+		prevlr += rdmNum
+		path.curve.add_point(Vector2(prevlr*pl,-((i+1)*pl)-fadedist))		
+	
+	var newOrigin = Vector2(prevlr*pl, -(pl*lpp)-fadedist-150)
+	
 	if(recursionDepth!=1):
-		path.add_child(rec(recursionDepth-1,newOrigin))
+		var rdmNum = rng.randi_range(1,3)
+		rdmNum = 4 ##todo 
+		var neuepath
+		if rdmNum<3:
+			#links
+			neuepath = rec(recursionDepth-1,newOrigin, "LChild")
+			path.add_child(neuepath)
+			
+			#und rechts
+			if rdmNum==2:
+				neuepath = rec(recursionDepth-1,newOrigin, "RChild")
+				path.add_child(neuepath)
+				
+		else:
+			pass
+			# nur rechts
+			#neuepath = rec(recursionDepth-1,newOrigin, "RChild")
+			#path.add_child(neuepath)
+				
+		neuepath = rec(recursionDepth-1,newOrigin, "SChild")
+		path.add_child(neuepath)
+			
+	if name=="nofade":
+		name = "SChild"
+	path.name = name
 	return path
+	
 
 func getInstance() -> Path2D:
 	var scene = preload("res://scenes/visible_path.tscn") #preload geht aber load nit :huh:
--- scripts/Player.gd
+++ scripts/Player.gd
@@ -3,7 +3,7 @@ extends PathFollow2D
 @onready var game_manager = $"/root/GameManager"
 @onready var camera = $Camera2D
 
-var speed = 100
+var speed = 800
 var zoom_fact = 0.001
 var current_lane = 1
 
@@ -13,55 +13,6 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	# The player is the child of the current path
-	# The parent of the current path therefore contains other valid paths
-	var current_path_layer = get_parent().get_parent()
-	var next_path_layer = get_parent()
-	
-	# Get all valid path indexes on the current layer
-	var parallel_paths = []
-	for child in current_path_layer.get_children():
-		if child is Path2D:
-			parallel_paths.append(child.get_index())
-	
-	# Switch to left or right sibling of current path on input
-	if Input.is_action_just_pressed("ui_left"):
-		if current_lane > 0:
-			current_lane -= 1
-			reparent(current_path_layer.get_child(parallel_paths[current_lane]))
-	if Input.is_action_just_pressed("ui_right"):
-		if current_lane < parallel_paths.size() - 1:
-			current_lane += 1
-			reparent(current_path_layer.get_child(parallel_paths[current_lane]))
-	
-	# Calculate progress along path according to speed
-	var change = delta * speed * game_manager.tick_speed
-	var change_ratio  = change / get_parent().curve.get_baked_length()
-	
-	# If progress would exceed current path, reparent to its child
-	if progress_ratio + change_ratio >= 1:
-		# Get all valid path indexes on the next layer
-		var next_paths = []
-		for child in next_path_layer.get_children():
-			if child is Path2D:
-				next_paths.append(child.get_index())
-				
-		# If there is another path to continue to, reparent
-		if next_paths.size() > 0:
-			progress += change
-			var next = get_parent().get_child(next_paths[next_paths.size()/2])
-			reparent(next)
-			progress_ratio -= 1
-		else:
-			pass
-	else:
-		# Increase progress normally
-		progress += change
-		
-	# Adjust camera zoom according to speed
-	camera.zoom.x = 1 / (speed * game_manager.tick_speed * zoom_fact + 1)
-	camera.zoom.y = 1 / (speed * game_manager.tick_speed * zoom_fact + 1)
-		
-	
+	pass
 	
 	
