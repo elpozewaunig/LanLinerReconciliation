b29251fe569a0c4322634ce716f174f052d1a11b
Seal <88384688+Seal187@users.noreply.github.com>
2024-07-20 00:59:16 +0200

    PathTreeGenV1

--- scenes/PathBuilder.gd
+++ scenes/PathBuilder.gd
@@ -1,12 +1,15 @@
 extends Node2D
 
 
-var pl = 250 # how long is a path-line in px
+var pl = 150 # how long is a path-line in px
 var gap = 80 # how many px gap between the duplis 
-var ec = 3 # how many path elements
-var lpp = 2 # how many lines per path? (additional to asfo)
-var asfo = 2 # how many lines after a split to fade out into a normal path
+var dc = 4 # how many extra duplicates
+var ec = 3 # how many path elements (recursion!) careful with that
+var lpp = 3 # how many lines per path? (additional to asfo)
+var asfo = 3 # how many lines after a split to fade out into a normal path
 var rng = RandomNumberGenerator.new()
+var debuggap = 0
+var debugmove = 1500
 
 func createPath():
 	var root = Node2D.new()
@@ -15,13 +18,14 @@ func createPath():
 	lane.add_child(neuepath)
 	lane.name = "lane0"
 	root.add_child(lane)
-	for i in 4:
+	for i in dc:
 		var dupli = lane.duplicate()
 		dupli.position = Vector2(dupli.position.x-(gap*(i+1)), dupli.position.y)
 		dupli.name = "lane"+str(i+1)
 		root.add_child(dupli)
 	
 	root.name = "root"
+	root.position = Vector2(root.position.x,root.position.y+debugmove)
 	return root
 
 func rec(recursionDepth: int, origin: Vector2, name:String):
@@ -37,7 +41,7 @@ func rec(recursionDepth: int, origin: Vector2, name:String):
 	if(name=="LChild"):
 		for i in asfo:
 			path.curve.add_point(Vector2(-(i+1)*pl,-(pl*(i+1))))	
-		prevlr += -1*asfo
+		prevlr -= 1*asfo
 	elif(name=="RChild"):
 		for i in asfo:
 			path.curve.add_point(Vector2((i+1)*pl,-(pl*(i+1))))	
@@ -57,11 +61,10 @@ func rec(recursionDepth: int, origin: Vector2, name:String):
 		prevlr += rdmNum
 		path.curve.add_point(Vector2(prevlr*pl,-((i+1)*pl)-fadedist))		
 	
-	var newOrigin = Vector2(prevlr*pl, -(pl*lpp)-fadedist-150)
+	var newOrigin = Vector2(prevlr*pl, -(pl*lpp)-fadedist-debuggap)
 	
 	if(recursionDepth!=1):
 		var rdmNum = rng.randi_range(1,3)
-		rdmNum = 4 ##todo 
 		var neuepath
 		if rdmNum<3:
 			#links
@@ -76,8 +79,8 @@ func rec(recursionDepth: int, origin: Vector2, name:String):
 		else:
 			pass
 			# nur rechts
-			#neuepath = rec(recursionDepth-1,newOrigin, "RChild")
-			#path.add_child(neuepath)
+			neuepath = rec(recursionDepth-1,newOrigin, "RChild")
+			path.add_child(neuepath)
 				
 		neuepath = rec(recursionDepth-1,newOrigin, "SChild")
 		path.add_child(neuepath)
