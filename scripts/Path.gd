e59a261d006defba86c661c36508ba22f222c9b7
Seal <88384688+Seal187@users.noreply.github.com>
2024-07-20 03:54:13 +0200

    seal

--- scripts/Path.gd
+++ scripts/Path.gd
@@ -15,26 +15,38 @@ func _process(delta):
 
 func _draw():
 	
+	
+	
 	draw_polyline(curve.get_baked_points(), Color.WHITE, 5, true)
 
 	if(!tubele.is_empty()):
 		for el in tubele:
-			var newc = Curve2D.new()
+			var newc
 			var l1 = float(el[0])
 			var l2 = float(el[1])
 			var col = Color.DEEP_PINK
 			if(str(el[2])=="speed"):
-				col = Color.DARK_ORANGE
+				newc = getSpeedLine()
 			elif(str(el[2])=="slow"):
-				col = Color.DARK_CYAN				
+				newc = getSlowLine()				
 				
 			newc.add_point(curve.sample_baked(l1))
 			for pointInbetween in getPointsInBetween(l1,l2, curve):
-				pass#newc.add_point(pointInbetween)
+				newc.add_point(pointInbetween)
 			newc.add_point(curve.sample_baked(l2))
-			draw_polyline(newc.get_baked_points(),col,10,true)
+			#draw_polyline(newc.get_baked_points(),col,10,true)
+			self.add_child(newc)
 			
 			
+
+func getSpeedLine() -> Line2D:
+	var scene = preload("res://scenes/speedLine.tscn") #preload geht aber load nit :huh:
+	var line = scene.instantiate()
+	return line
+func getSlowLine() -> Line2D:
+	var scene = preload("res://scenes/slowLine.tscn") #preload geht aber load nit :huh:
+	var line = scene.instantiate()
+	return line	
 func getPointsInBetween(l1, l2, c):
 	var arr = []
 	var dist = 0.0
