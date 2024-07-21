078004a007345e6f5e2181ec98c7fcc56e34b0be
Elias Pozewaunig <64259275+elpozewaunig@users.noreply.github.com>
2024-07-21 16:57:26 +0200

    Implemented retry button
    Ugly code duplication because I got an error when reusing ButtonStart.gd???

--- scripts/ButtonStart.gd
+++ scripts/ButtonStart.gd
@@ -1,11 +1,10 @@
 extends Area2D
 
 @onready var label = $Label
-@onready var this_scene = $"/root/Menu"
 
 var mouse_inside = false
 
-var main_scene = preload("res://scenes/main.tscn").instantiate()
+var main_scene = preload("res://scenes/main.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,14 +15,13 @@ func _process(delta):
 	if mouse_inside:
 		label.modulate = Color(255, 0, 0)
 		if Input.is_action_pressed("ui_click"):
-			get_tree().root.add_child(main_scene)
-			get_tree().root.remove_child(this_scene)
+			get_tree().change_scene_to_packed(main_scene)
 	else:
 		label.modulate = Color(255, 255, 255)
 
 
 func _on_mouse_entered():
-	mouse_inside = true	
+	mouse_inside = true
 
 func _on_mouse_exited():
 	mouse_inside = false
---
+++ scripts/RetryButton.gd
@@ -0,0 +1,27 @@
+extends Area2D
+
+@onready var label = $Label
+
+var mouse_inside = false
+
+var main_scene = preload("res://scenes/main.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if mouse_inside:
+		label.modulate = Color(255, 0, 0)
+		if Input.is_action_pressed("ui_click"):
+			get_tree().change_scene_to_packed(main_scene)
+	else:
+		label.modulate = Color(255, 255, 255)
+
+
+func _on_mouse_entered():
+	mouse_inside = true
+
+func _on_mouse_exited():
+	mouse_inside = false
