elpozewaunig/LanLinerReconciliation

commit 4f335283aee7d37365a474dd9f806495061095c4

seal approved

scenes/enemy.tscn
@@ -1,9 +1,11 @@
-[gd_scene load_steps=12 format=3 uid="uid://4cpqid7g3o70"]
+[gd_scene load_steps=14 format=3 uid="uid://4cpqid7g3o70"]
 
 [ext_resource type="Script" path="res://scripts/Enemy.gd" id="1_037od"]
 [ext_resource type="Texture2D" uid="uid://qn5wy2v6josb" path="res://assets/enemy.png" id="2_mkgco"]
 [ext_resource type="Texture2D" uid="uid://dy3v7b64edpb6" path="res://assets/trialsheetenemy.png" id="3_l8ft8"]
 [ext_resource type="Script" path="res://scripts/TrailAnimation.gd" id="4_38np1"]
+[ext_resource type="Texture2D" uid="uid://cp0oaawxqu400" path="res://assets/twirl_03.png" id="5_kle1l"]
+[ext_resource type="Texture2D" uid="uid://b868fu7vn0f6e" path="res://assets/spark_04.png" id="6_av215"]
 
 [sub_resource type="CanvasItemMaterial" id="CanvasItemMaterial_lk7ji"]
 
@@ -77,4 +79,61 @@ scale = Vector2(0.565, 0.401)
 sprite_frames = SubResource("SpriteFrames_be6dp")
 script = ExtResource("4_38np1")
 
-[connection signal="end_reached" from="." to="." method="_on_end_reached"]
+[node name="ParticleSysMoveLeft" type="Node2D" parent="."]
+
+[node name="CPUParticles2D" type="CPUParticles2D" parent="ParticleSysMoveLeft"]
+amount = 1
+lifetime = 1.5
+speed_scale = 2.0
+texture = ExtResource("5_kle1l")
+emission_shape = 1
+emission_sphere_radius = 0.01
+gravity = Vector2(0, 0)
+angular_velocity_min = 150.0
+angular_velocity_max = 150.0
+scale_amount_min = 0.45
+scale_amount_max = 0.45
+color = Color(0.443137, 0, 0.0117647, 1)
+
+[node name="CPUParticles2D2" type="CPUParticles2D" parent="ParticleSysMoveLeft"]
+amount = 16
+lifetime = 10.0
+explosiveness = 0.12
+texture = ExtResource("6_av215")
+gravity = Vector2(260.18, 0)
+initial_velocity_max = 36.9
+angular_velocity_max = 169.2
+scale_amount_min = 0.35
+scale_amount_max = 0.35
+color = Color(0.443137, 0, 0.0117647, 1)
+
+[node name="ParticleSysMoveRight" type="Node2D" parent="."]
+visible = false
+
+[node name="CPUParticles2D" type="CPUParticles2D" parent="ParticleSysMoveRight"]
+visible = false
+amount = 1
+lifetime = 39.0
+speed_scale = 2.0
+texture = ExtResource("5_kle1l")
+emission_shape = 1
+emission_sphere_radius = 0.01
+gravity = Vector2(0, 0)
+angular_velocity_min = -150.0
+angular_velocity_max = -150.0
+scale_amount_min = 0.45
+scale_amount_max = 0.45
+color = Color(0.444371, 0, 0.0100586, 1)
+
+[node name="CPUParticles2D2" type="CPUParticles2D" parent="ParticleSysMoveRight"]
+visible = false
+amount = 16
+lifetime = 10.0
+explosiveness = 0.12
+texture = ExtResource("6_av215")
+gravity = Vector2(-260.18, 0)
+initial_velocity_max = 36.9
+angular_velocity_max = 169.2
+scale_amount_min = 0.35
+scale_amount_max = 0.35
+color = Color(0.443137, 0, 0.0117647, 1)

scenes/player.tscn
@@ -1,20 +1,16 @@
-[gd_scene load_steps=18 format=3 uid="uid://dc7aktr1f5opg"]
+[gd_scene load_steps=17 format=3 uid="uid://dc7aktr1f5opg"]
 
 [ext_resource type="Script" path="res://scripts/Player.gd" id="1_gyboe"]
 [ext_resource type="Script" path="res://scripts/Camera.gd" id="3_16twk"]
 [ext_resource type="Texture2D" uid="uid://bv2en2yvl0kvo" path="res://assets/vignette.png" id="3_74b6m"]
 [ext_resource type="Texture2D" uid="uid://crf3x1a6mm6b5" path="res://assets/player.png" id="3_oj2jm"]
 [ext_resource type="Texture2D" uid="uid://clplopyknmamn" path="res://assets/trialsheet.png" id="4_gc2fj"]
-[ext_resource type="Script" path="res://scripts/TimerDisplay.gd" id="4_gy5f6"]
-[ext_resource type="FontFile" uid="uid://deh6c62ayb1de" path="res://assets/fonts/Kenney Mini.ttf" id="4_rq0y7"]
 [ext_resource type="Script" path="res://scripts/TrailAnimation.gd" id="5_3ppbk"]
 [ext_resource type="PackedScene" uid="uid://cmhaktjvtnwyh" path="res://scenes/control_overlay.tscn" id="7_wphgy"]
+[ext_resource type="Texture2D" uid="uid://cp0oaawxqu400" path="res://assets/twirl_03.png" id="8_15hpb"]
+[ext_resource type="Texture2D" uid="uid://b868fu7vn0f6e" path="res://assets/spark_04.png" id="9_6d0nb"]
 
 [sub_resource type="CanvasItemMaterial" id="CanvasItemMaterial_lk7ji"]
-
-[sub_resource type="LabelSettings" id="LabelSettings_nx51t"]
-font = ExtResource("4_rq0y7")
-font_size = 120
 
 [sub_resource type="AtlasTexture" id="AtlasTexture_c61c1"]
 atlas = ExtResource("4_gc2fj")
@@ -70,52 +66,6 @@ script = ExtResource("3_16twk")
 [node name="Vignette" type="Sprite2D" parent="Camera2D"]
 texture = ExtResource("3_74b6m")
 
-[node name="TimerDisplay" type="Control" parent="Camera2D"]
-layout_mode = 3
-anchors_preset = 8
-anchor_left = 0.5
-anchor_top = 0.5
-anchor_right = 0.5
-anchor_bottom = 0.5
-grow_horizontal = 2
-grow_vertical = 2
-script = ExtResource("4_gy5f6")
-
-[node name="Time" type="Label" parent="Camera2D/TimerDisplay"]
-visible = false
-layout_mode = 1
-anchors_preset = 8
-anchor_left = 0.5
-anchor_top = 0.5
-anchor_right = 0.5
-anchor_bottom = 0.5
-offset_left = -127.5
-offset_top = -75.0
-offset_right = 127.5
-offset_bottom = 75.0
-grow_horizontal = 2
-grow_vertical = 2
-text = "Time"
-label_settings = SubResource("LabelSettings_nx51t")
-
-[node name="EnemyTime" type="Label" parent="Camera2D/TimerDisplay"]
-visible = false
-modulate = Color(1, 0, 0, 1)
-layout_mode = 1
-anchors_preset = 8
-anchor_left = 0.5
-anchor_top = 0.5
-anchor_right = 0.5
-anchor_bottom = 0.5
-offset_left = -127.5
-offset_top = -75.0
-offset_right = 127.5
-offset_bottom = 75.0
-grow_horizontal = 2
-grow_vertical = 2
-text = "Time"
-label_settings = SubResource("LabelSettings_nx51t")
-
 [node name="Sprites" type="Node2D" parent="."]
 
 [node name="Triangle" type="Sprite2D" parent="Sprites"]
@@ -143,6 +93,61 @@ script = ExtResource("5_3ppbk")
 
 [node name="ControlOverlay" parent="." instance=ExtResource("7_wphgy")]
 
+[node name="ParticleSysMoveLeft" type="Node2D" parent="."]
+visible = false
+
+[node name="CPUParticles2D" type="CPUParticles2D" parent="ParticleSysMoveLeft"]
+amount = 1
+lifetime = 1.5
+speed_scale = 2.0
+texture = ExtResource("8_15hpb")
+emission_shape = 1
+emission_sphere_radius = 0.01
+gravity = Vector2(0, 0)
+angular_velocity_min = 150.0
+angular_velocity_max = 150.0
+scale_amount_min = 0.45
+scale_amount_max = 0.45
+color = Color(0.519225, 0.764319, 0.7809, 1)
+
+[node name="CPUParticles2D2" type="CPUParticles2D" parent="ParticleSysMoveLeft"]
+amount = 16
+lifetime = 10.0
+explosiveness = 0.12
+texture = ExtResource("9_6d0nb")
+gravity = Vector2(260.18, 0)
+initial_velocity_max = 36.9
+angular_velocity_max = 169.2
+scale_amount_min = 0.35
+scale_amount_max = 0.35
+color = Color(0.693722, 0.855936, 0.902581, 1)
+
+[node name="ParticleSysMoveRight" type="Node2D" parent="."]
+
+[node name="CPUParticles2D" type="CPUParticles2D" parent="ParticleSysMoveRight"]
+amount = 1
+lifetime = 39.0
+speed_scale = 2.0
+texture = ExtResource("8_15hpb")
+emission_shape = 1
+emission_sphere_radius = 0.01
+gravity = Vector2(0, 0)
+angular_velocity_min = -150.0
+angular_velocity_max = -150.0
+scale_amount_min = 0.45
+scale_amount_max = 0.45
+color = Color(0.519225, 0.764319, 0.7809, 1)
+
+[node name="CPUParticles2D2" type="CPUParticles2D" parent="ParticleSysMoveRight"]
+amount = 16
+lifetime = 10.0
+explosiveness = 0.12
+texture = ExtResource("9_6d0nb")
+gravity = Vector2(-260.18, 0)
+initial_velocity_max = 36.9
+angular_velocity_max = 169.2
+scale_amount_min = 0.35
+scale_amount_max = 0.35
+color = Color(0.693722, 0.855936, 0.902581, 1)
+
 [connection signal="choice_btn_pressed" from="." to="ControlOverlay" method="_on_choice_btn_pressed"]
-[connection signal="end_reached" from="." to="Camera2D/TimerDisplay" method="_on_player_end_reached"]
-[connection signal="enemy_end_reached" from="." to="Camera2D/TimerDisplay" method="_on_enemy_end_reached"]

scripts/PathBuilder.gd
@@ -1,75 +1,0 @@
-extends Node2D
-
-
-var pl = 150 # how long is a path-line in px
-var gap = 160 # how many px gap between the duplis 
-var dc = 4 # how many extra duplicates
-var ec = 5 # how many path elements (recursion!) careful with that
-var lpp = 24 # how many lines per path? (additional to asfo)
-var asfo = 10 # how many lines after a split to fade out into a normal path ,sideways "fork"
-var asfof = 5 # after asfo, straight "fork" (SChild is straight for asfo+asfof)
-var bsfo = 4 # how many lines before a split to fade into a a splitter
-var rng = RandomNumberGenerator.new()
-var debuggap = 0
-var debugmove = 0#1500
-
-
-
-func createPath():
-	
-	
-func rec(recursionDepth: int, origin: Vector2, name:String):
-	
-	
-	
-	var path = getInstance()
-	
-
-	path.position = Vector2(origin.x,origin.y)
-	path.curve.add_point(Vector2(0,0))
-	var prevlr = 0
-	
-	var fadedist = asfo*pl
-	fadedist = asfo*pl
-	if(name=="LChild"):
-		for i in asfo:
-			path.curve.add_point(Vector2(-(i+1)*pl,-(pl*(i+1))))	
-		prevlr -= 1*asfo
-	elif(name=="RChild"):
-		for i in asfo:
-			path.curve.add_point(Vector2((i+1)*pl,-(pl*(i+1))))	
-		prevlr += 1*asfo
-		
-	elif(name=="SChild"):
-		for i in asfo:
-			path.curve.add_point(Vector2(0,-((i+1)*pl)))
-		
-	else:
-		fadedist = 0
-		path.curve.add_point(Vector2(0,0))
-	
-	for i in asfof:
-		path.curve.add_point(Vector2(prevlr*pl,-((i+1)*pl)-fadedist))
-	
-	fadedist+=asfof*pl
-	
-	
-	for i in lpp:
-		var rdmNum
-		if(name=="LChild"): 
-			rdmNum = rng.randi_range(-1,0)
-		elif(name=="SChild"):
-			rdmNum = rng.randi_range(-1,1)
-		elif(name=="RChild"):
-			rdmNum = rng.randi_range(0,1)
-		else:
-			rdmNum = 0
-		prevlr += rdmNum
-		path.curve.add_point(Vector2(prevlr*pl,-((i+1)*pl)-fadedist))		
-	
-	for i in bsfo:
-		path.curve.add_point(Vector2(prevlr*pl, (-(pl*lpp)-fadedist)-((i+1)*pl)))
-	
-	var newOrigin = Vector2(prevlr*pl, -(pl*lpp)-fadedist-(bsfo*pl)-debuggap)
-
-	

scripts/PathBuilderV2.gd
@@ -12,10 +12,12 @@ func _process(delta):
 
 var rng = RandomNumberGenerator.new()
 
+var lineLength = 800 #(alles ist min. 1 unit und wird damit multiplied)
+
 var recursions = 3
 var extraDuplicates = 4
-var gapBetweenDuplicates = 80
-
+var gapBetweenDuplicatesX = 80
+var gapBetweenDuplicatesY = 0
 func createPath():
 	var root = Node2D.new()
 	var lane = Node2D.new()
@@ -25,7 +27,9 @@ func createPath():
 	root.add_child(lane)
 	for i in extraDuplicates:
 		var dupli = lane.duplicate()
-		dupli.position = Vector2(dupli.position.x+(gapBetweenDuplicates*(i+1)), dupli.position.y)
+		dupli.position = Vector2(
+			dupli.position.x+(gapBetweenDuplicatesX*(i+1)), 
+			dupli.position.y+(gapBetweenDuplicatesY*(i+1)))
 		dupli.name = "lane"+str(i+1)
 		root.add_child(dupli)
 	
@@ -92,54 +96,55 @@ func chooseRandomS(c, x, y):
 	if(rdmNum==3): ## todo
 		return pSWavey(c, x, y)
 func chooseRandomC(c, x,y, vorzeichen):
-	var rdmNum = rng.randi_range(1,1)
+	var rdmNum = rng.randi_range(2,2)
 	if(rdmNum==1):
 		return pCFork(c, x, y, vorzeichen)
 	elif(rdmNum==2):
 		return pC90Turn(c, x, y, vorzeichen)
-	elif(rdmNum==3):
-		return pCdoubleUTurn(c, x, y, vorzeichen)
-	
+
 func chooseStart(c,x,y):
 	return startPathPatch(c,x,y)
 	
 func startPathPatch(c,x,y):
-	var s = Vector2(x,y-500)
+	var s = Vector2(x,y-lineLength*1)
 	c.add_point(s)
 	return s
 	
 func pSVanilla(c,x,y):
-	var s = Vector2(x,y-2000)
+	var s = Vector2(x,y-lineLength*3)
 	c.add_point(s)
 	return s
 	
+
+
 func pSZigZag(c,x,y):
 	var a = paddingS(c,x,y)
 	x = a.x
 	y = a.y
-	var paddingstraightBefore = 0
-	var paddingStraightAfter = 0
 	var flip = getRandomSign()
-	var l = 2000
+	c.add_point(Vector2(x,y))
+	
+	var l = lineLength*3
 	var w = 120
 	var divisions = 8
 	var dfac = l/divisions
-	c.add_point(Vector2(x,y-paddingstraightBefore))
-	c.add_point(Vector2(x,y-dfac/2-paddingstraightBefore))
-	for i in divisions:
-		c.add_point(Vector2(x+90*flip,y-(dfac*(i+1))-paddingstraightBefore))
-		
+
+	var preflipX = x
+	for i in divisions-1:
+		x=w*flip
+		y-=dfac
+		c.add_point(Vector2(x,y))
 		flip*=-1
-	c.add_point(Vector2(x,y-l-(dfac/2)-paddingstraightBefore))
-	c.add_point(Vector2(x,y-l-(dfac/2)-paddingstraightBefore-paddingStraightAfter))
-	y = y-l-paddingstraightBefore-paddingStraightAfter
-	var b = paddingS(c,x,y)
-	x = b.x
-	y = b.y
-	return Vector2(x,y)
+	
+	x = preflipX
+	y-=dfac
+	c.add_point(Vector2(x,y))
+	return paddingS(c,x,y)
 
 func paddingS(c,x,y):
-	return Vector2(x,y-250)
+	var a = Vector2(x,y-lineLength)
+	c.add_point(a)
+	return a
 
 func pSWavey(c,x,y): ##todo
 	return Vector2(x,y-500)
@@ -147,38 +152,27 @@ func pSWavey(c,x,y): ##todo
 
 
 func pCFork(c, x, y, sign): # y -750
-	x+=500*sign
-	y-=500
+	x+=lineLength*sign
+	y-=lineLength
 	c.add_point(Vector2(x,y))
 	var a = paddingS(c,x,y)
-	return chooseRandomS(c,a.x,a.y)
-
-func pC90Turn(c,x,y,sign):
-	x+=500*sign
-	y-=500
-	c.add_point(Vector2(x,y))
-	x+=2500*sign
-	c.add_point(Vector2(x,y))
-	x+=500*sign
-	y-=500
-	c.add_point(Vector2(x,y))
+	x = a.x
+	y = a.y
 	return Vector2(x,y)
 
-func pCdoubleUTurn(c,x,y,sign):
-	x+=500*sign
-	y-=500
+func pC90Turn(c,x,y,sign):
+	x+=lineLength*sign
+	y-=lineLength
 	c.add_point(Vector2(x,y))
-	x+=1000*sign
+	x+=lineLength*sign
+	y+=lineLength
 	c.add_point(Vector2(x,y))
-	x+=500*sign
-	y+=500
+	x+=lineLength*sign
+	y-=lineLength
 	c.add_point(Vector2(x,y))
-	y+=500
-	c.add_point(Vector2(x,y))
-	x+=500*-sign
-	y+=500
-	c.add_point(Vector2(x,y))
-	y+=1500
+	var a = paddingS(c,x,y)
+	x = a.x
+	y = a.y
 	return Vector2(x,y)
 
 func getRandomSign():

commit a568caa49af811446d899d3b0792abadaf4ae4df

Resolved problems introduced by 4f335283aee7d37365a474dd9f806495061095c4

scenes/Menu.tscn
@@ -1,4 +1,4 @@
-[gd_scene load_steps=17 format=3 uid="uid://b3quwpomtjty3"]
+[gd_scene load_steps=17 format=3 uid="uid://b7vjspmp3umjn"]
 
 [ext_resource type="FontFile" uid="uid://deh6c62ayb1de" path="res://assets/fonts/Kenney Mini.ttf" id="1_dgplp"]
 [ext_resource type="AudioStream" uid="uid://ddg7hgj0pltor" path="res://assets/sounds/blueprint_melody.ogg" id="1_kisyv"]
@@ -7,7 +7,7 @@
 [ext_resource type="Script" path="res://scripts/CreditsButton.gd" id="4_s67s6"]
 [ext_resource type="Script" path="res://scenes/DancingTitle.gd" id="5_8w2s3"]
 [ext_resource type="Script" path="res://scripts/CreditsExitButton.gd" id="5_fkbxm"]
-[ext_resource type="Texture2D" uid="uid://05l885rvsqsd" path="res://assets/x.png" id="6_6tsrf"]
+[ext_resource type="Texture2D" path="res://assets/x.png" id="6_6tsrf"]
 
 [sub_resource type="RectangleShape2D" id="RectangleShape2D_m3eyg"]
 size = Vector2(432, 95)
@@ -183,6 +183,7 @@ label_settings = SubResource("LabelSettings_n3wgl")
 horizontal_alignment = 1
 
 [node name="Tech" type="Label" parent="UI/Credits"]
+layout_mode = 0
 offset_top = 862.0
 offset_right = 1911.0
 offset_bottom = 1003.0

scenes/enemy.tscn
@@ -1,11 +1,9 @@
-[gd_scene load_steps=14 format=3 uid="uid://4cpqid7g3o70"]
+[gd_scene load_steps=12 format=3 uid="uid://4cpqid7g3o70"]
 
 [ext_resource type="Script" path="res://scripts/Enemy.gd" id="1_037od"]
 [ext_resource type="Texture2D" uid="uid://qn5wy2v6josb" path="res://assets/enemy.png" id="2_mkgco"]
 [ext_resource type="Texture2D" uid="uid://dy3v7b64edpb6" path="res://assets/trialsheetenemy.png" id="3_l8ft8"]
 [ext_resource type="Script" path="res://scripts/TrailAnimation.gd" id="4_38np1"]
-[ext_resource type="Texture2D" uid="uid://cp0oaawxqu400" path="res://assets/twirl_03.png" id="5_kle1l"]
-[ext_resource type="Texture2D" uid="uid://b868fu7vn0f6e" path="res://assets/spark_04.png" id="6_av215"]
 
 [sub_resource type="CanvasItemMaterial" id="CanvasItemMaterial_lk7ji"]
 
@@ -79,61 +77,4 @@ scale = Vector2(0.565, 0.401)
 sprite_frames = SubResource("SpriteFrames_be6dp")
 script = ExtResource("4_38np1")
 
-[node name="ParticleSysMoveLeft" type="Node2D" parent="."]
-
-[node name="CPUParticles2D" type="CPUParticles2D" parent="ParticleSysMoveLeft"]
-amount = 1
-lifetime = 1.5
-speed_scale = 2.0
-texture = ExtResource("5_kle1l")
-emission_shape = 1
-emission_sphere_radius = 0.01
-gravity = Vector2(0, 0)
-angular_velocity_min = 150.0
-angular_velocity_max = 150.0
-scale_amount_min = 0.45
-scale_amount_max = 0.45
-color = Color(0.443137, 0, 0.0117647, 1)
-
-[node name="CPUParticles2D2" type="CPUParticles2D" parent="ParticleSysMoveLeft"]
-amount = 16
-lifetime = 10.0
-explosiveness = 0.12
-texture = ExtResource("6_av215")
-gravity = Vector2(260.18, 0)
-initial_velocity_max = 36.9
-angular_velocity_max = 169.2
-scale_amount_min = 0.35
-scale_amount_max = 0.35
-color = Color(0.443137, 0, 0.0117647, 1)
-
-[node name="ParticleSysMoveRight" type="Node2D" parent="."]
-visible = false
-
-[node name="CPUParticles2D" type="CPUParticles2D" parent="ParticleSysMoveRight"]
-visible = false
-amount = 1
-lifetime = 39.0
-speed_scale = 2.0
-texture = ExtResource("5_kle1l")
-emission_shape = 1
-emission_sphere_radius = 0.01
-gravity = Vector2(0, 0)
-angular_velocity_min = -150.0
-angular_velocity_max = -150.0
-scale_amount_min = 0.45
-scale_amount_max = 0.45
-color = Color(0.444371, 0, 0.0100586, 1)
-
-[node name="CPUParticles2D2" type="CPUParticles2D" parent="ParticleSysMoveRight"]
-visible = false
-amount = 16
-lifetime = 10.0
-explosiveness = 0.12
-texture = ExtResource("6_av215")
-gravity = Vector2(-260.18, 0)
-initial_velocity_max = 36.9
-angular_velocity_max = 169.2
-scale_amount_min = 0.35
-scale_amount_max = 0.35
-color = Color(0.443137, 0, 0.0117647, 1)
+[connection signal="end_reached" from="." to="." method="_on_end_reached"]

scenes/player.tscn
@@ -1,16 +1,20 @@
-[gd_scene load_steps=17 format=3 uid="uid://dc7aktr1f5opg"]
+[gd_scene load_steps=18 format=3 uid="uid://dc7aktr1f5opg"]
 
 [ext_resource type="Script" path="res://scripts/Player.gd" id="1_gyboe"]
 [ext_resource type="Script" path="res://scripts/Camera.gd" id="3_16twk"]
 [ext_resource type="Texture2D" uid="uid://bv2en2yvl0kvo" path="res://assets/vignette.png" id="3_74b6m"]
 [ext_resource type="Texture2D" uid="uid://crf3x1a6mm6b5" path="res://assets/player.png" id="3_oj2jm"]
 [ext_resource type="Texture2D" uid="uid://clplopyknmamn" path="res://assets/trialsheet.png" id="4_gc2fj"]
+[ext_resource type="Script" path="res://scripts/TimerDisplay.gd" id="4_gy5f6"]
+[ext_resource type="FontFile" uid="uid://deh6c62ayb1de" path="res://assets/fonts/Kenney Mini.ttf" id="4_rq0y7"]
 [ext_resource type="Script" path="res://scripts/TrailAnimation.gd" id="5_3ppbk"]
 [ext_resource type="PackedScene" uid="uid://cmhaktjvtnwyh" path="res://scenes/control_overlay.tscn" id="7_wphgy"]
-[ext_resource type="Texture2D" uid="uid://cp0oaawxqu400" path="res://assets/twirl_03.png" id="8_15hpb"]
-[ext_resource type="Texture2D" uid="uid://b868fu7vn0f6e" path="res://assets/spark_04.png" id="9_6d0nb"]
 
 [sub_resource type="CanvasItemMaterial" id="CanvasItemMaterial_lk7ji"]
+
+[sub_resource type="LabelSettings" id="LabelSettings_nx51t"]
+font = ExtResource("4_rq0y7")
+font_size = 120
 
 [sub_resource type="AtlasTexture" id="AtlasTexture_c61c1"]
 atlas = ExtResource("4_gc2fj")
@@ -66,6 +70,52 @@ script = ExtResource("3_16twk")
 [node name="Vignette" type="Sprite2D" parent="Camera2D"]
 texture = ExtResource("3_74b6m")
 
+[node name="TimerDisplay" type="Control" parent="Camera2D"]
+layout_mode = 3
+anchors_preset = 8
+anchor_left = 0.5
+anchor_top = 0.5
+anchor_right = 0.5
+anchor_bottom = 0.5
+grow_horizontal = 2
+grow_vertical = 2
+script = ExtResource("4_gy5f6")
+
+[node name="Time" type="Label" parent="Camera2D/TimerDisplay"]
+visible = false
+layout_mode = 1
+anchors_preset = 8
+anchor_left = 0.5
+anchor_top = 0.5
+anchor_right = 0.5
+anchor_bottom = 0.5
+offset_left = -127.5
+offset_top = -75.0
+offset_right = 127.5
+offset_bottom = 75.0
+grow_horizontal = 2
+grow_vertical = 2
+text = "Time"
+label_settings = SubResource("LabelSettings_nx51t")
+
+[node name="EnemyTime" type="Label" parent="Camera2D/TimerDisplay"]
+visible = false
+modulate = Color(1, 0, 0, 1)
+layout_mode = 1
+anchors_preset = 8
+anchor_left = 0.5
+anchor_top = 0.5
+anchor_right = 0.5
+anchor_bottom = 0.5
+offset_left = -127.5
+offset_top = -75.0
+offset_right = 127.5
+offset_bottom = 75.0
+grow_horizontal = 2
+grow_vertical = 2
+text = "Time"
+label_settings = SubResource("LabelSettings_nx51t")
+
 [node name="Sprites" type="Node2D" parent="."]
 
 [node name="Triangle" type="Sprite2D" parent="Sprites"]
@@ -93,61 +143,6 @@ script = ExtResource("5_3ppbk")
 
 [node name="ControlOverlay" parent="." instance=ExtResource("7_wphgy")]
 
-[node name="ParticleSysMoveLeft" type="Node2D" parent="."]
-visible = false
-
-[node name="CPUParticles2D" type="CPUParticles2D" parent="ParticleSysMoveLeft"]
-amount = 1
-lifetime = 1.5
-speed_scale = 2.0
-texture = ExtResource("8_15hpb")
-emission_shape = 1
-emission_sphere_radius = 0.01
-gravity = Vector2(0, 0)
-angular_velocity_min = 150.0
-angular_velocity_max = 150.0
-scale_amount_min = 0.45
-scale_amount_max = 0.45
-color = Color(0.519225, 0.764319, 0.7809, 1)
-
-[node name="CPUParticles2D2" type="CPUParticles2D" parent="ParticleSysMoveLeft"]
-amount = 16
-lifetime = 10.0
-explosiveness = 0.12
-texture = ExtResource("9_6d0nb")
-gravity = Vector2(260.18, 0)
-initial_velocity_max = 36.9
-angular_velocity_max = 169.2
-scale_amount_min = 0.35
-scale_amount_max = 0.35
-color = Color(0.693722, 0.855936, 0.902581, 1)
-
-[node name="ParticleSysMoveRight" type="Node2D" parent="."]
-
-[node name="CPUParticles2D" type="CPUParticles2D" parent="ParticleSysMoveRight"]
-amount = 1
-lifetime = 39.0
-speed_scale = 2.0
-texture = ExtResource("8_15hpb")
-emission_shape = 1
-emission_sphere_radius = 0.01
-gravity = Vector2(0, 0)
-angular_velocity_min = -150.0
-angular_velocity_max = -150.0
-scale_amount_min = 0.45
-scale_amount_max = 0.45
-color = Color(0.519225, 0.764319, 0.7809, 1)
-
-[node name="CPUParticles2D2" type="CPUParticles2D" parent="ParticleSysMoveRight"]
-amount = 16
-lifetime = 10.0
-explosiveness = 0.12
-texture = ExtResource("9_6d0nb")
-gravity = Vector2(-260.18, 0)
-initial_velocity_max = 36.9
-angular_velocity_max = 169.2
-scale_amount_min = 0.35
-scale_amount_max = 0.35
-color = Color(0.693722, 0.855936, 0.902581, 1)
-
 [connection signal="choice_btn_pressed" from="." to="ControlOverlay" method="_on_choice_btn_pressed"]
+[connection signal="end_reached" from="." to="Camera2D/TimerDisplay" method="_on_player_end_reached"]
+[connection signal="enemy_end_reached" from="." to="Camera2D/TimerDisplay" method="_on_enemy_end_reached"]

scripts/ControlOverlay.gd
@@ -1,8 +1,5 @@
 extends Node2D
 
-@onready var game_manager = $"/root/GameManager"
-@onready var player = game_manager.player
-
 @onready var left = $Left
 @onready var right = $Right
 @onready var up = $Up