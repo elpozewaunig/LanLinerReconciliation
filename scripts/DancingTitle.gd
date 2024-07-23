e2f558f8b7e9b3ea3edd354926ec73ac8552a8f8
Elias Pozewaunig <64259275+elpozewaunig@users.noreply.github.com>
2024-07-23 19:09:57 +0200

    Made dancing title easily configurable

--- scripts/DancingTitle.gd
+++ scripts/DancingTitle.gd
@@ -1,5 +1,7 @@
 extends Label
 
+@export var max_rotation_deg = 3
+@export var rotation_speed = 10
 var direction = 1
 
 # Called when the node enters the scene tree for the first time.
@@ -9,12 +11,12 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if rotation_degrees < 3 and direction == 1:
-		rotation_degrees += delta * 10
-		if rotation_degrees >= 3:
+	if rotation_degrees < max_rotation_deg and direction == 1:
+		rotation_degrees += delta * rotation_speed
+		if rotation_degrees >= max_rotation_deg:
 			direction = -1
 			
-	if rotation_degrees > -3 and direction == -1:
-		rotation_degrees -= delta * 10
-		if rotation_degrees <= -3:
+	if rotation_degrees > -1 * max_rotation_deg and direction == -1:
+		rotation_degrees -= delta * rotation_speed
+		if rotation_degrees <= -1 * max_rotation_deg:
 			direction = 1
